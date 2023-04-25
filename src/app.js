ff882400dbe80236a000781e94b87fe9f4d53496
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-04-24 21:07:29 -0400

    profile view

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ const express = require('express');
 
 const app = express();
 
-const accountData = fs.readFileSync('./json/accounts.json', 'utf-8');
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync('./json/users.json', 'utf-8');
-const users = JSON.parse(userData);
-
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+
+const accountData = fs.readFileSync('./json/accounts.json', 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('./json/users.json', 'utf8');
+const users = JSON.parse(userData);
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Account Summary', accounts: accounts });
@@ -22,10 +22,6 @@ app.get('/savings', function (req, res) {
   res.render('account', { account: accounts.savings });
 });
 
-app.get('/profile', function (req, res) {
-  res.render('profile', { user: users[0] });
-});
-
 app.get('/checking', function (req, res) {
   res.render('account', { account: accounts.savings });
 });
@@ -33,6 +29,11 @@ app.get('/checking', function (req, res) {
 app.get('/credit', function (req, res) {
   res.render('account', { account: accounts.savings });
 });
+
+app.get('/profile', function (req, res) {
+  res.render('profile', { user: users[0] });
+});
+
 app.listen(3000, function () {
   console.log('PS Project Running on port 3000!');
 });
