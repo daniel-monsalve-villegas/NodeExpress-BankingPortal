28afb913b3369d4b9af6e9f9f64ccf0168a94b82
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-04-25 13:12:44 -0400

    transition user data to data library

--- src/data.js
+++ src/data.js
@@ -3,3 +3,7 @@ const path = require('path');
 
 const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
 const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', 'utf8');
+const users = JSON.parse(userData);
+
