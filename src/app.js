95bd3f9b46f0efdb5ebeb6378e6d66e28a42b73c
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-04-24 23:01:00 -0400

    Convert Account Data to JSON

--- src/app.js
+++ src/app.js
@@ -44,10 +44,10 @@ app
       accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance =
       parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-    const accounstJSON = JSON.stringify(accounts, null, 4);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
     fs.writeFileSync(
       path.join(__dirname, 'json/accounts.json'),
-      accounstJSON,
+      accountsJSON,
       'utf8'
     );
     res.render('transfer', { message: 'Transfer Completed' });
