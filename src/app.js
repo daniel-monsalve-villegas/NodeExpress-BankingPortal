7267598cb97c1cdd23761b3bbcf07c8844b0b6b1
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-04-25 13:30:12 -0400

    require data library

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { accounts, users, writeJSON } = require('./data');
 
 const app = express();
 
@@ -9,11 +10,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync('src/json/users.json', 'utf8');
-const users = JSON.parse(userData);
+/* const accountData = fs.readFileSync('src/json/accounts.json', 'utf8'); */
+/* const accounts = JSON.parse(accountData); */
+/**/
+/* const userData = fs.readFileSync('src/json/users.json', 'utf8'); */
+/* const users = JSON.parse(userData); */
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Account Summary', accounts: accounts });
